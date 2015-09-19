060b816bf9fdaba9413234586809ea910d0474a9
wikijm <jmarc.alb@laposte.net>
2015-09-19 19:42:39 +0200

    Use functions defined on external .ps1 files

---
+++ ActiveDirectory/ListADObjectsOnSelectedOU/ListADObjectsOnSelectedOU.ps1
@@ -0,0 +1,112 @@
+﻿#requires -version 2
+<#
+.SYNOPSIS
+  List all AD Objects on a selected OU, and export results into a CSV
+
+.DESCRIPTION
+  List all AD objects on a graphically selected OU, and export results into a CSV (CommonName and Description)
+
+.INPUTS
+  User Interface
+
+.OUTPUTS
+   Create transcript log file similar to $ScriptDir\[SCRIPTNAME]_[YYYY_MM_DD]_[HHhMMmSSs].log
+   Create a list of AD Objects, similar to $ScriptDir\[SCRIPTNAME]_FoundADObjects_[YYYY_MM_DD]_[HHhMMmSSs].csv
+   
+   
+.NOTES
+  Version:        1.1
+  Author:         ALBERT Jean-Marc
+  Creation Date:  15/07/2015
+  Purpose/Change: 1.0 - 2015.07.15 - ALBERT Jean-Marc - Initial script development
+                  1.1 - 2015.07.16 - ALBERT Jean-Marc - Optimize code (minification)
+                  1.2 - 2015.07.17 - ALBERT Jean-Marc - Add sAMAccountName and homedirectory to "Get-ADObject parameters"
+                  1.3 - 2015.09.12 - ALBERT Jean-Marc - Use functions defined on external .ps1 files
+                  
+                                                  
+.SOURCES
+  
+  
+.EXAMPLE
+  <None>
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+Set-StrictMode -version Latest
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$scriptFile = $MyInvocation.MyCommand.Definition
+$launchDate = get-date -f "yyyy.MM.dd-HHhmmmss"
+$logDirectoryPath = $scriptPath + "\" + $launchDate
+$buffer = "$scriptPath\bufferCommand.txt"
+$fullScriptPath = (Resolve-Path -Path $buffer).Path
+
+$loggingFunctions = "$scriptPath\logging\Logging.ps1"
+$utilsFunctions = "$scriptPath\utilities\Utils.ps1"
+$addsFunctions = "$scriptPath\utilities\ADDS.ps1"
+
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+$scriptName = [System.IO.Path]::GetFileName($scriptFile)
+$scriptVersion = "1.3"
+
+if(!(Test-Path $logDirectoryPath)) {
+    New-Item $logDirectoryPath -type directory | Out-Null
+}
+
+$logFileName = "Log_" + $launchDate + ".log"
+$logPathName = "$logDirectoryPath\$logFileName"
+
+$global:streamWriter = New-Object System.IO.StreamWriter $logPathName
+
+#Define CSV file export of AD Group objects
+$CSVExportADObjects = "$ScriptPath\$scriptFile" + "_" + "FoundADObjects" + "_" + $launchDate + ".csv"
+
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+. $loggingFunctions
+. $utilsFunctions
+. $addsFunctions
+
+
+#----------------------------------------------------------[Execution]----------------------------------------------------------
+
+Start-Log -scriptName $scriptName -scriptVersion $scriptVersion -streamWriter $global:streamWriter
+cls
+Write-Host "================================================================================================"
+
+# Prerequisites
+if($adminFlag -eq $false){
+    Write-Host "You have to launch this script with " -nonewline; Write-Host "local Administrator rights!" -f Red    
+    $scriptPath = Split-Path $MyInvocation.InvocationName    
+    $RWMC = $scriptPath + "\$scriptName.ps1"
+    $ArgumentList = 'Start-Process -FilePath powershell.exe -ArgumentList \"-ExecutionPolicy Bypass -File "{0}"\" -Verb Runas' -f $RWMC;
+    Start-Process -FilePath powershell.exe -ArgumentList $ArgumentList -Wait -NoNewWindow;    
+    Stop-Script
+}
+
+Write-Host "================================================================================================"
+
+ # Execute action with a progressbar
+ Write-Progress -Activity "Starting GUI to select OU" -status "Running..." -id 1 
+ # Call OnApplicationLoad to initialize
+   If((OnApplicationLoad) -eq $true) {
+	     #Call the form
+	     Call-AD_OU_select_pff | Out-Null
+	     #Perform cleanup
+	     OnApplicationExit
+                                     }
+
+ # Show selected OU on a messagebox
+   [System.Windows.Forms.MessageBox]::Show("Selected OU: $objSelectedOU" , "Information" , 0, [Windows.Forms.MessageBoxIcon]::Information)
+
+
+ # Writing informations in the log file
+ Write-Progress -Activity "Write informations in the log file" -status "Running..." -id 1
+ 
+ End-Log -streamWriter $global:streamWriter
+ Stop-ScriptMessageBox
+ notepad $logPathName
+ cls
---
+++ ActiveDirectory/ListADObjectsOnSelectedOU/utilities/Utils.ps1
@@ -0,0 +1,117 @@
+﻿function OnApplicationLoad {
+	return $true #return true for success or false for failure
+                           }
+function OnApplicationExit {
+	$script:ExitCode = 0 #Set the exit code for the Packager
+                           }
+
+function Set-RegistryKey($computername, $parentKey, $nameRegistryKey, $valueRegistryKey) {
+    try{    
+        $remoteBaseKeyObject = [microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computername)     
+        $regKey = $remoteBaseKeyObject.OpenSubKey($parentKey,$true)
+        $regKey.Setvalue("$nameRegistryKey", "$valueRegistryKey", [Microsoft.Win32.RegistryValueKind]::DWORD) 
+        $remoteBaseKeyObject.close()
+    }
+    catch {
+        $_.Exception
+    }
+}
+
+function Disable-UAC($computername) {
+    $parentKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System"
+    $nameRegistryKey = "LocalAccountTokenFilterPolicy"
+    $valueRegistryKey = "1"
+
+    $objReg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $computername)
+    $objRegKey= $objReg.OpenSubKey($parentKey)
+    $test = $objRegkey.GetValue($nameRegistryKey)
+    if($test -eq $null){    
+        Set-RegistryKey $computername $parentKey $nameRegistryKey $valueRegistryKey     
+        Write-Host "Registry key setted, you have to reboot the remote computer" -foregroundcolor "magenta"
+        Stop-Script
+    }
+    else {
+        if($test -ne 1){
+            Set-RegistryKey $computername $parentKey $nameRegistryKey $valueRegistryKey     
+            Write-Host "Registry key setted, you have to reboot the remote computer" -foregroundcolor "magenta"
+            Stop-Script
+        }
+    }
+}
+
+function CreateDirectoryIfNeeded ( [string] $directory ) {
+	if (!(Test-Path -Path $directory -type "Container")) {
+		New-Item -type directory -Path $directory > $null
+	}
+}
+
+function Run-WmiRemoteProcess {
+    Param(
+        [string]$computername=$env:COMPUTERNAME,
+        [string]$cmd=$(Throw "You must enter the full path to the command which will create the process."),
+        [int]$timeout = 0
+    )
+ 
+    Write-Host "Process to create on $computername is $cmd"
+    [wmiclass]$wmi="\\$computername\root\cimv2:win32_process"
+    # Exit if the object didn't get created
+    if (!$wmi) {return}
+ 
+    try{
+    $remote=$wmi.Create($cmd)
+    }
+    catch{
+        $_.Exception
+    }
+    $test =$remote.returnvalue
+    if ($remote.returnvalue -eq 0) {
+        Write-Host ("Successfully launched $cmd on $computername with a process id of " + $remote.processid)
+    } else {
+        Write-Host ("Failed to launch $cmd on $computername. ReturnValue is " + $remote.ReturnValue)
+    }    
+    return
+}
+
+function Stop-Script () {   
+    Begin{
+        Write-Log -streamWriter $global:streamWriter -infoToLog "--- Script terminating ---"
+    }
+    Process{        
+        "Script terminating..." 
+        Write-Host "================================================================================================"
+        End-Log -streamWriter $global:streamWriter       
+        Exit
+    }
+}
+
+Function Stop-ScriptMessageBox () {
+ # MessageBox who inform of the end of the process
+   [System.Windows.Forms.MessageBox]::Show(
+"Process done.
+The log file will be opened when click on 'OK' button.
+Please, check the log file for further informations.
+" , "End of process" , 0, [Windows.Forms.MessageBoxIcon]::Information)
+}
+
+function Test-InternetConnection {
+    if(![Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}')).IsConnectedToInternet){
+        Write-Host "The script need an Internet Connection to run" -f Red    
+        Stop-Script
+    }
+}
+
+function Test-LocalAdminRights {
+    $myComputer = Get-WMIObject Win32_ComputerSystem | Select-Object -ExpandProperty name
+    $myUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $amIAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent())
+    $adminFlag = $amIAdmin.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+    if($adminFlag -eq $true){
+        $adminMessage = " with administrator rights on " 
+    }
+    else {
+        $adminMessage = " without administrator rights on "
+    }
+
+    Write-Host "RWMC runs with user " -nonewline; Write-Host $myUser.Name -f Red -nonewline; Write-Host $adminMessage -nonewline; Write-Host $myComputer -f Red -nonewline; Write-Host " computer"
+    return $adminFlag
+}
