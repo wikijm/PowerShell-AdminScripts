8c65950545a402ecc0ff5525776bbe11dcc38030
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2017-01-10 13:50:18 +0100

    Redesign script with PowerShell-ScriptTemplate.ps1

--- Office-365/Set-DefaultUserAsRoomsCalenderReviewer.ps1
+++ Office-365/Set-DefaultUserAsRoomsCalenderReviewer.ps1
@@ -1,7 +1,112 @@
-# Work in progress for this script, be careful with it!
+﻿#requires -version 2
+<#
+.SYNOPSIS
+  <None>
 
-function Connect-EXOnline
-{
+.DESCRIPTION
+  <None>
+
+.INPUTS
+  <None>
+
+.OUTPUTS
+  Create transcript log file similar to $ScriptDir\[SCRIPTNAME]_[YYYY_MM_DD]_[HHhMMmSSs].log
+  Create a list of SUBJECT, similar to $ScriptDir\[SCRIPTNAME]_SUBJECT_[YYYY_MM_DD]_[HHhMMmSSs].csv
+   
+   
+.NOTES
+  Version:        1.1
+  Author:         ALBERT Jean-Marc
+  Creation Date:  10/01/2017 (DD/MM/YYYY)
+  Purpose/Change: 1.0 - 2017.01.10 - ALBERT Jean-Marc - Initial script development
+                  1.1 - 2017.01.10 - ALBERT Jean-Marc - Redesign script with PowerShell-ScriptTemplate.ps1
+                
+                    
+                                                  
+.SOURCES
+  <None>
+  
+  
+.EXAMPLE
+  <None>
+
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+Set-StrictMode -version Latest
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$scriptFile = $MyInvocation.MyCommand.Definition
+$launchDate = get-date -f "yyyy.MM.dd-HHhmmmss"
+$logDirectoryPath = $scriptPath + "\" + $launchDate
+
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+$scriptName = [System.IO.Path]::GetFileName($scriptFile)
+$scriptVersion = "1.1"
+
+if(!(Test-Path $logDirectoryPath)) {
+    New-Item $logDirectoryPath -type directory | Out-Null
+}
+
+$logFileName = "Log_" + $launchDate + ".log"
+$logPathName = "$logDirectoryPath\$logFileName"
+
+$global:streamWriter = New-Object System.IO.StreamWriter $logPathName
+
+#-----------------------------------------------------------[functions]------------------------------------------------------------
+
+function Start-Log {    
+    [CmdletBinding()]  
+    Param ([Parameter(Mandatory=$true)][string]$scriptName, [Parameter(Mandatory=$true)][string]$scriptVersion, 
+        [Parameter(Mandatory=$true)][string]$streamWriter)
+    Process{                  
+        $global:streamWriter.WriteLine("================================================================================================")
+        $global:streamWriter.WriteLine("[$ScriptName] version [$ScriptVersion] started at $([DateTime]::Now)")
+        $global:streamWriter.WriteLine("================================================================================================`n")       
+    }
+}
+ 
+function Write-Log {
+    [CmdletBinding()]  
+    Param ([Parameter(Mandatory=$true)][string]$streamWriter, [Parameter(Mandatory=$true)][string]$infoToLog)  
+    Process{    
+        $InfoMessage = "$([DateTime]::Now) [INFO] $infoToLog"
+        $global:streamWriter.WriteLine($InfoMessage)
+        Write-Host $InfoMessage -ForegroundColor Cyan
+    }
+}
+ 
+function Write-Error {
+    [CmdletBinding()]  
+    Param ([Parameter(Mandatory=$true)][string]$streamWriter, [Parameter(Mandatory=$true)][string]$errorCaught, [Parameter(Mandatory=$true)][boolean]$forceExit)  
+    Process{
+        $ErrorMessage = "$([DateTime]::Now) [ERROR] $errorCaught"
+        $global:streamWriter.WriteLine($ErrorMessage)
+        Write-Host $ErrorMessage -ForegroundColor Red      
+        if ($forceExit -eq $true){
+            End-Log -streamWriter $global:streamWriter
+            break;
+        }
+    }
+}
+ 
+function End-Log { 
+    [CmdletBinding()]  
+    Param ([Parameter(Mandatory=$true)][string]$streamWriter)  
+    Process{    
+        $global:streamWriter.WriteLine("`n================================================================================================")
+        $global:streamWriter.WriteLine("Script ended at $([DateTime]::Now)")
+        $global:streamWriter.WriteLine("================================================================================================")
+  
+        $global:streamWriter.Close()   
+    }
+}
+
+function Connect-EXOnline {
 	#Define URL to contact Office 365
 	$Office365URL = "https://ps.outlook.com/powershell"
 	
@@ -21,23 +126,32 @@ function Connect-EXOnline
 	Import-PSSession $EXOSession
 }
 
-function Disconnect-EXOnline
-{
+function Disconnect-EXOnline {
 	Remove-PSSession -Name "Exchange Online"
 }
+
+#----------------------------------------------------------[Execution]----------------------------------------------------------
+
+Start-Log -scriptName $scriptName -scriptVersion $scriptVersion -streamWriter $global:streamWriter
+cls
+Write-Host "================================================================================================"
+Write-Host "[$ScriptName] version [$ScriptVersion] started at $([DateTime]::Now)"
+Write-Host "================================================================================================"
 
 # Disclaimer
 $Disclaimer = [Windows.Forms.MessageBox]::Show(
 	"
-This script aims to set Exchange/Office 365 users as Room mailbox calendar's reviewer.
+Ce script permet à tous les utilisateurs Exchange/Office 365 de devenir relecteur sur les calendriers de salle de réunion et équipements.
 
-									/!\ CAUTION /!\
-If you are not sure of the type of action to carry on, or the impact on the mailing service, please stop this script now.
-Do you wish to proceed?
-", 'Set Exchange/Office 365 users as Room mailbox calendar's reviewer', 1, [Windows.Forms.MessageBoxIcon]::Question)
+                                 /!\ Attention /!\
+Si vous n'êtes pas sûr des actions à mener ou de l'impact, quitter ce script dès à présent.
+
+Souhaitez-vous continuer ?
+
+", 'Modification de droits sur les calendriers de salle et équipement', 1, [Windows.Forms.MessageBoxIcon]::Question)
 If ($Disclaimer -eq "OK")
 {
-	Write-Information 'Please wait, work in progress...'
+	Write-Information 'Patientez, traitement en cours ...'
 }
 Else
 {
@@ -48,20 +162,32 @@ Else
 }
 
 
-
 # Start a connection to Office 365
 Connect-EXOnline
 
-# List all RoomMailbox
+# List all RoomMailbox and set Exchange/Office 365 users as calendar reviewer
 $rooms = Get-Mailbox -RecipientTypeDetails RoomMailbox
+$rooms | ForEach-Object {Write-Log -streamWriter $global:streamWriter -infoToLog "Room: $_"}
+$rooms | %{Set-MailboxFolderPermission $_":\Calendrier" -User Default -AccessRights Reviewer} | ForEach-Object {Write-Log -streamWriter $global:streamWriter -infoToLog $_} #Replace :\Calendrier with :\Calendar for English context
 
-# Set Exchange/Office 365 users as Room mailbox calendar's reviewer
-&rooms | %{Set-MailboxFolderPermission $_":\Calendar" -User Default -AccessRights Reviewer}
+# List all EquipmentMailbox and set Exchange/Office 365 users as calendar reviewer
+$equipments = Get-Mailbox -RecipientTypeDetails EquipmentMailbox
+$equipments | ForEach-Object {Write-Log -streamWriter $global:streamWriter -infoToLog "Equipment: $_"} 
+$equipments | %{Set-MailboxFolderPermission $_":\Calendrier" -User Default -AccessRights Reviewer} | ForEach-Object {Write-Log -streamWriter $global:streamWriter -infoToLog $_} #Replace :\Calendrier with :\Calendar for English context
+
+Write-Log -streamWriter $global:streamWriter -infoToLog $rooms
 
 [Windows.Forms.MessageBox]::Show(
 	"Action menée avec succès.
-", 'Boîte partagée', 0, [Windows.Forms.MessageBoxIcon]::Information)
+", 'Modification de droits sur les calendriers de salle et équipement', 0, [Windows.Forms.MessageBoxIcon]::Information)
 
 
 # Stop the connection to Office 365
 Disconnect-EXOnline
+
+
+#Writing informations in the log file
+Write-Progress -Activity "Write informations in the log file" -status "Running..." -id 1
+End-Log -streamWriter $global:streamWriter
+notepad $logPathName
+cls
