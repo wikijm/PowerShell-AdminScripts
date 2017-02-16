ceca6be824e17cfa4f6aa5850f2ae5dc71e22c9d
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2017-02-16 13:49:12 +0100

    Add DesktopViewer version check & enhance log
    - Add a verification on WebHelper version (new (2) parameter)
    - Redefine parameters
    - Add version of Citrix package and Citrix DesktopViewer module version on log

--- APP-Deploy/CITRIX-Receiver-14.6.0.12010-Install.bat
+++ APP-Deploy/CITRIX-Receiver-14.6.0.12010-Install.bat
@@ -1,19 +1,25 @@
-REM                                       
-REM  DATE:		Feb 10, 2017
-REM  VERSION:	0.1
-REM  INPUTS:	(1) Current version of package                
-REM		(2) Package Location/Deployment Directory     
-REM		(3) Script Logging Directory                  
-REM		(4) Package Installer Command Line Options
+REM                                                        
+REM  DATE:		Feb 16, 2017
+REM  VERSION:	0.3
+REM  INPUTS:	(1) Current version of Citrix package                
+REM		        (2) Current Version of Citrix WebHelper module
+REM		        (3) Package Location/Deployment Directory     
+REM      		  (4) Script Logging Directory                  
+REM		        (5) Package Installer Command Line Options
 REM
-REM  USAGE: Launch manually or use on 'Computer Configuration\Windows Settings\Scripts\Startup' GPO
-REM
-REM  CHANGELOG: v0.1 - 10.02.2017 - Initial write
+REM  CHANGELOG: v0.1 - 25.01.2017 - Initial write
+REM		           v0.2 - 10.02.2017 - Add ',DesktopViewer' to (4)
+REM	           	v0.3 - 16.02.2017 - Add a verification on WebHelper version (new (2) parameter)
+REM				                             Redefine parameters
+REM						                          	Add version of Citrix package and Citrix DesktopViewer module version on log
 REM
 REM                                                        
-REM  (1) Current version of package                        
+REM  (1) Current version of Citrix package                        
 Set DesiredVersion=14.6.0.12010
-REM                                                        
+REM
+REM  (2) Current version of Citrix WebHelper module
+Set DesiredDesktopViewerVersion=14.6.0.12010
+REM                                                
 REM  (2) Package Location/Deployment Directory             
 Set DeployDirectory=\\contorso.dom\apps\Citrix\
 REM                                                        
@@ -30,7 +36,7 @@ IF NOT "%ProgramFiles(x86)%"=="" SET WOW6432NODE=WOW6432NODE\
  
 REM Check if the Desired plug-in is installed
 REM
-reg query "HKEY_LOCAL_MACHINE\SOFTWARE\%WOW6432NODE%Citrix\PluginPackages\XenAppSuite\ICA_Client" | findstr %DesiredVersion%
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\%WOW6432NODE%Citrix\PluginPackages\XenAppSuite\DesktopViewer" | findstr %DesiredWebHelperVersion%
 if %errorlevel%==1 (goto NotFound) else (goto End)
 REM
 REM If 1 was returned, the registry query found the Desired Version is not installed.
@@ -45,8 +51,8 @@ start /wait %DeployDirectory%\CitrixReceiver%DesiredVersion%.exe DONOTSTARTCC=1 
 if %errorlevel% neq 0 (goto BadInstall) else (goto End)
 
 :BadInstall
-echo %date% %time% %ComputerName% [ERROR] Deployment ended with error code %errorlevel%. >>%logshare%global.log
+echo %date% %time% %ComputerName% [ERROR] %DesiredVersion% %DesiredDesktopViewerVersion% Deployment ended with error code %errorlevel%. >>%logshare%global.log
 
 :End
-echo %date% %time% %ComputerName% [INFO] Deployment ended with error code %errorlevel%. >>%logshare%global.log
+echo %date% %time% %ComputerName% [INFO] %DesiredVersion% %DesiredDesktopViewerVersion% Deployment ended with error code %errorlevel%. >>%logshare%global.log
 exit 0
