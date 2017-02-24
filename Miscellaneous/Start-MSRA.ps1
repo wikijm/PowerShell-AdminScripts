a0650e3f8455cd6a13ab53bb1b16d8c26748779e
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2017-02-24 13:50:40 +0100

    Automatize log's 1st line and show tech info
    - Automatically add first line (containing headers) on log file.
    - Show information concerning tech which execute the script

--- Miscellaneous/Start-MSRA.ps1
+++ Miscellaneous/Start-MSRA.ps1
@@ -3,6 +3,29 @@ $launchHour = get-date -f "HH:mm:ss"
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $logFileName = "pmad.log"
 $logPathName = "P:\Exploitation\Suivi\PMAD\$logFileName"
+$logFileFirstLine = "Date,Heure,PosteAdmin,Admin,Utilisateur,PosteUser,Ticket"
+
+
+function Set-FirstLine {
+
+    param (
+        [string]$Path,
+        [string]$Value
+    )
+    
+    $oldcontent = Get-Content $Path
+    Set-Content -Path $Path -Value $Value
+    Add-Content -Path $Path -Value $oldcontent
+    
+}
+
+
+$logFileFirstLineContent = Get-content $logPathName -First 1
+
+if($logFileFirstLineContent -ne $logFileFirstLine){
+    Set-FirstLine -Path $logPathName -Value $logFileFirstLine 
+}
+
 
 Add-Type -AssemblyName System.Windows.Forms
 
@@ -16,34 +39,53 @@ $Form.TopMost = $false
 $Form.Width = 460
 $Form.Height = 305
 
-$buttonStartRC = New-Object system.windows.Forms.Button
-$buttonStartRC.Text = "Lancer contrôle à distance"
-$buttonStartRC.Width = 142
-$buttonStartRC.Height = 52
-$buttonStartRC.Add_MouseClick({
-    $Result = "$env:COMPUTERNAME,$env:USERNAME,$($textBoxUtilisateur.Text),$($textBoxComputerName.Text),$($textBoxTicket.Text)"
-	$launchDate + ',' + $launchHour + ',' +$env:COMPUTERNAME + ',' + $env:USERNAME + ',' + $textBoxUtilisateur.Text + ',' + $textBoxComputerName.Text + ',' + $textBoxTicket.Text | Out-File -filepath $logPathName -Append -Encoding UTF8
-    msra.exe /offerRA $textBoxComputerName.Text
-})
-$buttonStartRC.location = new-object system.drawing.point(223,157)
-$buttonStartRC.Font = "Microsoft Sans Serif,10"
-$Form.controls.Add($buttonStartRC)
+$labelDescription = New-Object system.windows.Forms.Label
+$labelDescription.Text = "Merci de remplir les champs ci-dessous afin de prendre la main
+sur un poste Windows.
+
+Tous sont obligatoires."
+$labelDescription.AutoSize = $true
+$labelDescription.Width = 25
+$labelDescription.Height = 10
+$labelDescription.location = new-object system.drawing.point(10,20)
+$labelDescription.Font = "Microsoft Sans Serif,10"
+$Form.controls.Add($labelDescription)
+
+
+$LabelProd = New-Object system.windows.Forms.Label
+$LabelProd.Text = "Administrateur"
+$LabelProd.AutoSize = $true
+$LabelProd.Width = 25
+$LabelProd.Height = 10
+$LabelProd.location = new-object system.drawing.point(24,101)
+$LabelProd.Font = "Microsoft Sans Serif,10"
+$Form.controls.Add($LabelProd)
+
+$textBoxProd = New-Object system.windows.Forms.TextBox
+$textBoxProd.Text = $env:USERNAME
+$textBoxProd.Enabled = $false
+$textBoxProd.Width = 100
+$textBoxProd.Height = 20
+$textBoxProd.location = new-object system.drawing.point(120,101)
+$textBoxProd.Font = "Microsoft Sans Serif,10"
+$Form.controls.Add($textBoxProd)
+
+$textBoxProdComputer = New-Object system.windows.Forms.TextBox
+$textBoxProdComputer.Text = $env:COMPUTERNAME
+$textBoxProdComputer.Enabled = $false
+$textBoxProdComputer.Width = 100
+$textBoxProdComputer.Height = 20
+$textBoxProdComputer.location = new-object system.drawing.point(265,101)
+$textBoxProdComputer.Font = "Microsoft Sans Serif,10"
+$Form.controls.Add($textBoxProdComputer)
 
 $textBoxUtilisateur = New-Object system.windows.Forms.TextBox
 $textBoxUtilisateur.Text = "xxxN"
 $textBoxUtilisateur.Width = 100
 $textBoxUtilisateur.Height = 20
-$textBoxUtilisateur.location = new-object system.drawing.point(101,151)
+$textBoxUtilisateur.location = new-object system.drawing.point(120,151)
 $textBoxUtilisateur.Font = "Microsoft Sans Serif,10"
 $Form.controls.Add($textBoxUtilisateur)
-
-$textBoxComputerName = New-Object system.windows.Forms.TextBox
-$textBoxComputerName.Text = "Uxnnnnnnnnn"
-$textBoxComputerName.Width = 100
-$textBoxComputerName.Height = 20
-$textBoxComputerName.location = new-object system.drawing.point(101,189)
-$textBoxComputerName.Font = "Microsoft Sans Serif,10"
-$Form.controls.Add($textBoxComputerName)
 
 $LabelUtilisateur = New-Object system.windows.Forms.Label
 $LabelUtilisateur.Text = "Utilisateur"
@@ -63,12 +105,13 @@ $LabelComputerName.location = new-object system.drawing.point(23,188)
 $LabelComputerName.Font = "Microsoft Sans Serif,10"
 $Form.controls.Add($LabelComputerName)
 
-$textBoxTicket = New-Object system.windows.Forms.TextBox
-$textBoxTicket.Width = 100
-$textBoxTicket.Height = 20
-$textBoxTicket.location = new-object system.drawing.point(98,230)
-$textBoxTicket.Font = "Microsoft Sans Serif,10"
-$Form.controls.Add($textBoxTicket)
+$textBoxComputerName = New-Object system.windows.Forms.TextBox
+$textBoxComputerName.Text = "Uxnnnnnnnnn"
+$textBoxComputerName.Width = 100
+$textBoxComputerName.Height = 20
+$textBoxComputerName.location = new-object system.drawing.point(120,189)
+$textBoxComputerName.Font = "Microsoft Sans Serif,10"
+$Form.controls.Add($textBoxComputerName)
 
 $labelTicket = New-Object system.windows.Forms.Label
 $labelTicket.Text = "Ticket"
@@ -79,23 +122,31 @@ $labelTicket.location = new-object system.drawing.point(22,230)
 $labelTicket.Font = "Microsoft Sans Serif,10"
 $Form.controls.Add($labelTicket)
 
-$labelDescription = New-Object system.windows.Forms.Label
-$labelDescription.Text = "Merci de remplir les champs ci-dessous afin de prendre la main
-sur un poste COMPANY.
+$textBoxTicket = New-Object system.windows.Forms.TextBox
+$textBoxTicket.Width = 100
+$textBoxTicket.Height = 20
+$textBoxTicket.location = new-object system.drawing.point(120,230)
+$textBoxTicket.Font = "Microsoft Sans Serif,10"
+$Form.controls.Add($textBoxTicket)
 
-Tous sont obligatoires."
-$labelDescription.AutoSize = $true
-$labelDescription.Width = 25
-$labelDescription.Height = 10
-$labelDescription.location = new-object system.drawing.point(10,20)
-$labelDescription.Font = "Microsoft Sans Serif,10"
-$Form.controls.Add($labelDescription)
+$buttonStartRC = New-Object system.windows.Forms.Button
+$buttonStartRC.Text = "Lancer contrôle à distance"
+$buttonStartRC.Width = 142
+$buttonStartRC.Height = 52
+$buttonStartRC.Add_MouseClick({
+    $Result = "$env:COMPUTERNAME,$env:USERNAME,$($textBoxUtilisateur.Text),$($textBoxComputerName.Text),$($textBoxTicket.Text)"
+	$launchDate + ',' + $launchHour + ',' +$env:COMPUTERNAME + ',' + $env:USERNAME + ',' + $textBoxUtilisateur.Text + ',' + $textBoxComputerName.Text + ',' + $textBoxTicket.Text | Out-File -filepath $logPathName -Append -Encoding UTF8
+    msra.exe /offerRA $textBoxComputerName.Text
+})
+$buttonStartRC.location = new-object system.drawing.point(250,155)
+$buttonStartRC.Font = "Microsoft Sans Serif,10"
+$Form.controls.Add($buttonStartRC)
 
 $buttonLog = New-Object system.windows.Forms.Button
 $buttonLog.Text = "Log"
 $buttonLog.Width = 60
 $buttonLog.Height = 30
-$buttonLog.location = new-object system.drawing.point(264,214)
+$buttonLog.location = new-object system.drawing.point(292,220)
 $buttonLog.Font = "Microsoft Sans Serif,10"
 $Form.controls.Add($buttonLog)
 $buttonLog.Add_MouseClick({
