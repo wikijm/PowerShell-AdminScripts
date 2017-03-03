8d139a4664ab43235e4f570fece3e8a775aa21a1
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2017-03-03 15:02:22 +0100

    Modify way to request data

--- Domoticz/functions.ps1
+++ Domoticz/functions.ps1
@@ -1,17 +1,17 @@
 $DomoticzApiProtocol = 'http'
-$DomoticzApiIP = '192.168.0.xx'
+$DomoticzApiIP = '192.168.0.XX'
 $DomoticzApiPort = '8080'
-$DomoticzDeviceID = 'x'
+$DomoticzDeviceID = 'X'
 
 function Get-LightSwitchStatus {
     Param([Parameter(Mandatory=$true)][string]$DomoticzDeviceID)
     $DomoticzRequestURL = $DomoticzApiProtocol + '://' + $DomoticzApiIP + ':' + $DomoticzApiPort + '/json.htm?type=devices&rid=' + $DomoticzDeviceID
-    $DomoticzRequestResult = Invoke-WebRequest $DomoticzRequestURL | ConvertFrom-Json | Select -expand result
+    $DomoticzRequestResult = Invoke-WebRequest $DomoticzRequestURL | ConvertFrom-Json
 
-    Write-Host $DomoticzRequestResult.HardwareName
-    Write-Host $DomoticzRequestResult.Type
-    Write-Host $DomoticzRequestResult.Status
-    Write-Host $DomoticzRequestResult.LastUpdate
+Write-Host $DomoticzRequestResult.result.HardwareName
+Write-Host $DomoticzRequestResult.result.Type
+Write-Host $DomoticzRequestResult.result.Status
+Write-Host $DomoticzRequestResult.result.LastUpdate
 }
 
-Get-LightSwitchStatus $DomoticzDeviceID
+Get-LightSwitchStatus X
